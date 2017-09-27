84102b8e275dc28aa0108b2ac8b350e9a2223999
Marshall T. Rose <mrose17@gmail.com>
2017-09-27 13:55:31 -0700

    initial check-in

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const restfulClient = require('./lib/clients/restful-client');
+const websocketClient = require('./lib/clients/websocket-client');
+
+module.exports = {
+    restfulClient: restfulClient.RestfulClient,
+    websocketClient: websocketClient.WebsocketClient
+};
+
+
+
+
